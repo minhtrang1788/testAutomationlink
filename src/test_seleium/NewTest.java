2e82366ebda6a70dbd7126cfc0085a55779bf7b3
Administrator <minhtrang1788@gmail.com>
2018-11-01 10:01:17 +0700

    commit source

---
+++ src/test_seleium/NewTest.java
@@ -0,0 +1,39 @@
+package test_seleium;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+	WebDriver driver;
+  @Test
+  public void f() {
+	  String homePageTitle = driver.getTitle();
+		Assert.assertEquals(homePageTitle, "Google");
+	  
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  	System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		// Firefox
+		//driver = new FirefoxDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
